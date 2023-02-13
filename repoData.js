29fbb4bd2e43e74577cb481db5afdeea30de23ae
khanhmuy <maikhanhhuy0608@outlook.com>
2023-02-13 20:49:35 +0700

    man

---
+++ repoData.js
@@ -0,0 +1,12 @@
+//Add Name of your Repo here
+exports.name = 'Example Repo';
+//Add Name and Type of Image Icon you want to use here (Must be PNG)
+exports.icon = 'icon.png';
+//Add the folder name where all your emotes are saved
+exports.pathName = 'emotes';
+//Add your name here, or remove the line if you don't need your name displayed under the repo's title in our clients
+exports.author = 'Joe';
+//Add a Description here, useful for SEO and Web Crawlers, or sharing to social websites. Remove the line if you don't care about Sharing.
+exports.description = 'Example Description';
+//Add Keywords here, useful for SEO and Web Crawlers. Remove the line if you don't care about SEO/Web Crawlers.
+exports.keywords = 'Nitroless, nitroless, iOS, macOS, Android, Windows, Emotes, Discord, discord, Nitro, nitro';
